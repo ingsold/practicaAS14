9b64738fe66739080401c832a229c6001e5c5012
Oswaldo <Oswaldo@oocf-lap>
2015-10-10 11:40:42 -0600

    Clase Para empezar con el cifrado

---
+++ src/practica/Cifrar.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica;
+
+import com.sun.xml.internal.ws.util.StringUtils;
+import java.util.Arrays;
+
+/**
+ *
+ * @author Oswaldo
+ */
+public class Cifrar {
+
+    public Cifrar() {
+
+    }
+
+    public Cifrar(String texto, String clave) {
+        for (int x = 0; x < texto.length(); x++) {
+            int i = texto.codePointAt(x);
+            byte[] y = String.valueOf(texto.charAt(x)).getBytes();
+            System.out.println(texto.charAt(x) + " = " + texto.codePointAt(x));;
+            System.out.println(texto.codePointAt(x) % 128);
+            System.out.println(y.length);
+
+        }
+    }
+
+    public void agregarCeros(int i) {
+       // if (Integer.toBinaryString(i).length() < 8) {
+            String original = Integer.toBinaryString(i);
+            char c = '0';
+            int number = 8 - Integer.toBinaryString(i).length();
+            char[] repeat = new char[number];
+            Arrays.fill(repeat, c);
+            original = new String(repeat) + original;
+            System.out.println("ORGINAL " + original);
+        /*} else {
+            String original = Integer.toBinaryString(i);
+            System.out.println("ORGINAL " + original);
+        }*/
+    }
+
+    public static void main(String args[]) {
+        Cifrar c = new Cifrar();
+        c.agregarCeros(255);
+    }
+}
