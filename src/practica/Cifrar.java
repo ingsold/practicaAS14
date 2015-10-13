2c04db7535c76c03a5187cfd81c37b3c9987ac72
Oswaldo <Oswaldo@oocf-lap>
2015-10-13 00:02:07 -0600

    Ya cifra una parte falla en el segundo

--- src/practica/Cifrar.java
+++ src/practica/Cifrar.java
@@ -28,7 +28,7 @@ public class Cifrar {
         }
     }
 
-    public ArrayList<Integer> convertirAscii(String v){
+    public ArrayList<Integer> convertirAscii(String v) {
         ArrayList<Integer> salida = new ArrayList<>();
         for (int i = 0; i < v.length(); i++) {
             salida.add(v.codePointAt(i));
@@ -36,15 +36,17 @@ public class Cifrar {
         }
         return salida;
     }
-    
+
     public String agregarCeros(int i) {
         String original = Integer.toBinaryString(i);
-        char c = '0';
-        int number = 8 - Integer.toBinaryString(i).length();
-        char[] repeat = new char[number];
-        Arrays.fill(repeat, c);
-        original = new String(repeat) + original;
-        //System.out.println("ORGINAL " + original);
+        if (original.length() < 8) {
+            char c = '0';
+            int number = 8 - Integer.toBinaryString(i).length();
+            char[] repeat = new char[number];
+            Arrays.fill(repeat, c);
+            original = new String(repeat) + original;
+            //System.out.println("ORGINAL " + original);
+        }
         return original;
     }
 
@@ -56,43 +58,44 @@ public class Cifrar {
         entrada += new String(repeat);
         return entrada;
     }
-    
-    public ArrayList<Integer> rotacionCircular(ArrayList<Integer> entrada){
+
+    public ArrayList<Integer> rotacionCircular(ArrayList<Integer> entrada) {
         ArrayList<Integer> salida = new ArrayList<>();
         char[] mover = new char[64];
         for (int k = 0; k < entrada.size(); k++) {
-            String a =agregarCeros(entrada.get(k));
-            char[] carac =a.toCharArray();
-            System.arraycopy(carac, 0, mover, k*8, 8);
+            String a = agregarCeros(entrada.get(k));
+            char[] carac = a.toCharArray();
+            System.arraycopy(carac, 0, mover, k * 8, 8);
             //System.out.println("CLAVE COPIADA "+Arrays.toString(mover));
         }
-        
-        //System.out.println("CLAVE ANTES"+Arrays.toString(mover));
-        mover=mueveClave(mover);
-        int parcial=0;
-        for (int i = 0; i < 64; i++) {
-            parcial+=(Integer.valueOf(String.valueOf(mover[i])))*2;
-            //System.out.println("conversion parcial: "+parcial+" "+mover[i]);
-            if (i%7==0){
+
+        //System.out.println("CLAVE ANTES  "+Arrays.toString(mover));
+        mover = mueveClave(mover);
+        //System.out.println("CLAVE DESPUES"+Arrays.toString(mover));
+        int parcial = 0;
+        for (int i = 1; i <= 64; i++) {
+            parcial = (parcial * 2) + Integer.valueOf(String.valueOf(mover[i - 1]));
+            //System.out.println("i%8=" +(i%8)+"conversion parcial: "+parcial+" "+mover[i-1]+" "+String.valueOf(mover[i-1]));
+            //System.out.println("i%7=" +(i%7));
+            if (i % 8 == 0 && i > 0) {
                 salida.add(parcial);
-                System.out.println("conversion parcial: "+parcial);
-                parcial=(Integer.valueOf(String.valueOf(mover[i])));
+                //System.out.println("conversion final: "+parcial+" "+mover[i-1]+" "+String.valueOf(mover[i-1]));
+                parcial = (Integer.valueOf(String.valueOf(mover[i - 1])));
             }
-            
+
         }
-        return null;
+        //System.out.println("salida size: " + salida.size());
+        return salida;
     }
+
     public char[] mueveClave(char[] clave) {
         char[] clavenueva = new char[64];
-        //System.arraycopy(clavenueva, 63, clave, 1, 63);
-        //System.arraycopy(this, srcPos, this, destPos, length);
-        
         for (int i = 0; i < 63; i++) {
-            clavenueva[i] = clave[i+1];
+            clavenueva[i] = clave[i + 1];
         }
-        clavenueva[63]=clave[0];
-        System.out.println("clave anterior:" + Arrays.toString(clave).replace(" ", ""));
-        System.out.println("clave nueva   :" + Arrays.toString(clavenueva).replace(" ", ""));
+        clavenueva[63] = clave[0];
+        //System.out.println("clave anterior:" + Arrays.toString(clave).replace(" ", ""));
+        //System.out.println("clave nueva   :" + Arrays.toString(clavenueva).replace(" ", ""));
         return clavenueva;
     }
 
--- src/practica/Principal.java
+++ src/practica/Principal.java
@@ -15,7 +15,7 @@ public class Principal extends javax.swing.JFrame {
 
     String parse;
     ArrayList<String> datos = new ArrayList<>();
-    String claveEntrada="";
+    String claveEntrada = "";
 
     /**
      * Creates new form Principal
@@ -176,73 +176,77 @@ public class Principal extends javax.swing.JFrame {
     }//GEN-LAST:event_txtClaveActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        
+
         Cifrar c = new Cifrar();
         datos = dividirTexto(txtAreaEntrada.getText());
         System.out.println(datos.size());
-        
+
         //Revisar clave
-        if(txtClave.getText().length()==8){
-            claveEntrada=txtClave.getText();
-        }else if(txtClave.getText().length()<8){
-            claveEntrada=c.agregarEspacios(txtClave.getText(),8);
-        }else{
-            claveEntrada=txtClave.getText().substring(0, 8);
+        if (txtClave.getText().length() == 8) {
+            claveEntrada = txtClave.getText();
+        } else if (txtClave.getText().length() < 8) {
+            claveEntrada = c.agregarEspacios(txtClave.getText(), 8);
+        } else {
+            claveEntrada = txtClave.getText().substring(0, 8);
         }
-        System.out.println("Clave Entrada: "+claveEntrada);
-        ArrayList<Integer> cifrado=new ArrayList<>();
-        for (int i = 0; i<datos.size(); i++) {
+        System.out.println("Clave Entrada: " + claveEntrada);
+        ArrayList<Integer> cifrado = new ArrayList<>();
+        for (int i = 0; i < datos.size(); i++) {
             String dato = datos.get(i);
             //Paso 0. Agregar espacios si es necesario al texto
             if (dato.length() < 16) {
                 datos.remove(i);
-                datos.add(i,c.agregarEspacios(dato,16));
+                datos.add(i, c.agregarEspacios(dato, 16));
             }
-            
+
             //Paso 1. Texto a Ascii
             ArrayList<Integer> asciisTexto = c.convertirAscii(datos.get(i));
             ArrayList<Integer> asciisClave = c.convertirAscii(claveEntrada);
             //System.out.println("SIZES: "+asciisClave.size()+" "+ asciisTexto.size());
             //ArrayList<Integer> cifrado=new ArrayList<>(datos.size()*16);
             //Paso 2. Ascii operaciones
-            for (int j = 8; j < asciisTexto.size(); j++) {
-                Integer td = asciisTexto.get(j);
-                Integer cl = asciisClave.get(j-8);
-                Integer ti = asciisTexto.get(j-8);
-                System.out.println("Asciis "+td+" "+cl);
-                Integer result = td ^ cl;
-                System.out.println("Resultado: " + result);
-                System.out.println("Asciis "+result+" "+ti);
-                result =result^ ti;
-                System.out.println("Resultado: " + result);
-                cifrado.add((j-8)+((j-8)*i), result);
+            int pasos = 8;
+            while (pasos >= 0) {
+                for (int j = 8; j < asciisTexto.size(); j++) {
+                    Integer td = asciisTexto.get(j);
+                    Integer cl = asciisClave.get(j - 8);
+                    Integer ti = asciisTexto.get(j - 8);
+                    //System.out.println("Asciis " + td + " " + cl);
+                    Integer result = td ^ cl;
+                    //System.out.println("Resultado: " + result);
+                    //System.out.println("Asciis " + result + " " + ti);
+                    result = result ^ ti;
+                    //System.out.println("Resultado: " + result);
+                    cifrado.add((j - 8) + ((j - 8) * i), result);
+
+                }
+                for (int j = 8; j < asciisTexto.size(); j++) {
+                    Integer ti = asciisTexto.get(j - 8);
+                    cifrado.add(j + (j * i), ti);
+                }
+                //Mover circularmente la clave
+                asciisClave = c.rotacionCircular(asciisClave);
+                asciisTexto= new ArrayList<Integer>(cifrado); //esta linea la esta cagando
                 
+                imprimirParte(asciisTexto);
+                //imprimirParte(cifrado);
+                cifrado.clear();
+                pasos--;
             }
-            for (int j = 8; j < asciisTexto.size(); j++) {
-                Integer ti = asciisTexto.get(j-8);
-                cifrado.add(j+(j*i),ti);
-            }
-            
-            
-            imprimirParte(asciisTexto);
-            imprimirParte(cifrado);
-            
         }
-        
-        
-        
-        
+
 
     }//GEN-LAST:event_jButton1ActionPerformed
-    
-    public void imprimirParte(ArrayList<Integer> entrada){
+
+    public void imprimirParte(ArrayList<Integer> entrada) {
         for (Integer entrada1 : entrada) {
             //System.out.println("Ascii: " + entrada1);
             System.out.print(((char) entrada1.intValue()));
         }
         System.out.println("");
-        
+
     }
+
     public static ArrayList<String> dividirTexto(String text) {
         // Give the list the right capacity to start with. You could use an array
         // instead if you wanted.
