f69eff62db8028479cc288dc158f53632915126e
oocf <>
2015-10-13 12:42:44 -0600

    Ya va queriendo pero igual falla en la segunda iteracion al agregar un item

--- src/practica/Practica.java
+++ src/practica/Practica.java
@@ -5,7 +5,6 @@
  */
 package practica;
 
-import java.util.ArrayList;
 
 /**
  *
@@ -17,25 +16,9 @@ public class Practica {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       Cifrar c = new Cifrar();
-       /*char[] clav=new char[64] ;
-       for(int i=0;i<64;i++){
-           int letra=(int)(Math.random()*2);
-           clav[i]=Integer.toBinaryString(letra).toCharArray()[0];
-         //  System.out.print(Integer.toString(letra)+"--");
-       }*/
-       //clav=c.mueveClave(clav);
-       //clav=c.mueveClave(clav);
-       ArrayList<Integer> arr = new ArrayList<>();
-       arr.add(1);
-       arr.add(2);
-       arr.add(3);
-       arr.add(4);
-       arr.add(5);
-       arr.add(6);
-       arr.add(7);
-       arr.add(8);
-       c.rotacionCircular((arr));
+       java.awt.EventQueue.invokeLater(() -> {
+            new Principal().setVisible(true);
+        });
     }
     
 }
